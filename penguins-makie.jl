32716fa9ea369d08faeee6e8cbf3d14d7ed67cef
Ari <ari.huttunen@me.com>
2021-04-30 14:36:48 +0300

    added df grouping method

--- penguins-makie.jl
+++ penguins-makie.jl
@@ -26,11 +26,26 @@ md"""
 # PalmerPenguins with Makie.jl
 """
 
+# ╔═╡ 5f11a9cb-8e79-4ff5-a556-7e23ffe6a9ac
+md"""
+This notebook illustrates how to plot PalmerPenguins with a few different combinations of DataFrames.jl, GLMakie.jl, CairoMakie.jl, Chain.jl, DataFramesMeta.jl.
+"""
+
+# ╔═╡ ec127dae-d2ac-42a6-9de6-948feee8c338
+md"""
+Get data about [palmerpenguins](https://allisonhorst.github.io/palmerpenguins/) via [PalmerPenguins.jl](https://github.com/devmotion/PalmerPenguins.jl).
+"""
+
 # ╔═╡ a46310e3-53d1-4bab-8b7c-b4bef16efcc9
 begin
 	penguins = dropmissing(DataFrame(PalmerPenguins.load()))
 	first(penguins,5)
 end
+
+# ╔═╡ 94f1ce39-a43e-4f25-9c4c-683509861d46
+md"""
+Set some generic attributes for plots via themes.
+"""
 
 # ╔═╡ 836fd8dd-f920-4975-a6a0-47da575b3eb7
 set_theme!(Theme(
@@ -45,30 +60,40 @@ set_theme!(Theme(
 ))
 
 
-# ╔═╡ bf10c94e-5b9e-463f-9c7d-c65b237e3166
-begin
-	adelie_style = (
+# ╔═╡ d5463461-e423-42fc-bb79-9ca68ed04715
+md"""
+Set some species related attributes; these will be used by splatting them on the plotting function.
+"""
+
+# ╔═╡ d650356c-9bb7-41cf-82f4-f62d1c2d0d56
+style = Dict(
+	"Adelie" => (
 		color=(:orange,0.5),  
 		strokecolor=:orange,
 		label="Adelie",
 		marker=:circle, 
 		markersize=20
-	)
-	chinstrap_style = (
+	),
+	"Chinstrap" => (
 		color=(:slateblue1,0.5),  
 		strokecolor=:slateblue1,
 		label="Chinstrap", 
 		marker=:utriangle, 
 		markersize=20
-	)
-	gentoo_style = (
+	),
+	"Gentoo" => (
 		color=(:turquoise3,0.5), 
 		strokecolor=:turquoise3,
 		label="Gentoo", 
 		marker=:diamond, 
 		markersize=20
 	)
-end
+);
+
+# ╔═╡ 0fbb46dd-b198-489a-a648-5b0468e1e6f8
+md"""
+Plotting one species at a time by using filter() and @chain.
+"""
 
 # ╔═╡ d6896864-73bd-4021-a166-d88a062a6293
 let
@@ -86,20 +111,50 @@ let
 		filter(:species => ==("Adelie"), _)
 		scatter!(ax1, 
 			_[!,:flipper_length_mm], _[!,:bill_length_mm];
-			adelie_style...)
+			style["Adelie"]...)
 	end
 	
 	@chain penguins begin
 		filter(:species => ==("Chinstrap"), _)
 		scatter!(ax1, _[!,:flipper_length_mm], _[!,:bill_length_mm]; 
-			chinstrap_style...)
+			style["Chinstrap"]...)
 	end
 	
 	@chain penguins begin
 		filter(:species => ==("Gentoo"), _)
 		scatter!(ax1, 
 			_[!,:flipper_length_mm], _[!,:bill_length_mm]; 
-			gentoo_style...)
+			style["Gentoo"]...)
+	end
+
+	axislegend("Gentoo", position = :rb)
+	
+	fig
+end
+
+# ╔═╡ 9f5d001f-e02c-4157-84b5-98f54528c97c
+md"""
+Plotting one species at a time by using groupby() to group the DataFrame by species. The output is identical.
+"""
+
+# ╔═╡ 0e3dd7e4-aa23-4a32-a127-ea22b978f2e0
+let
+	CairoMakie.activate!()
+	
+	fig = Figure(resolution = (1000, 700))
+	ax1 = fig[1, 1] = Axis(fig, 
+		title = "Flipper and bill length",
+		xlabel="Flipper length (mm)", 
+		ylabel="Bill length (mm)")
+	ax1.xticks = 160:10:240
+	ax1.yticks = 30:5:60
+	
+	for (i, gdf) in enumerate(groupby(penguins, :species, sort = true))
+    	scatter!(
+			gdf.flipper_length_mm, 
+			gdf.bill_length_mm; 
+			style[gdf.species[1]]...
+		)
 	end
 
 	axislegend("Species", position = :rb)
@@ -107,7 +162,12 @@ let
 	fig
 end
 
-# ╔═╡ 99e9388f-147e-4c26-af83-6ee32e6a546d
+# ╔═╡ 29535b0e-62da-4235-b50e-61376362c507
+md"""
+The same output, but plotted using GLMakie.jl. The look is slightly different.
+"""
+
+# ╔═╡ 77fcdbe4-7058-4fa4-a2b9-b72ea5f540d0
 let
 	GLMakie.activate!()
 	
@@ -119,24 +179,12 @@ let
 	ax1.xticks = 160:10:240
 	ax1.yticks = 30:5:60
 	
-	@chain penguins begin
-		filter(:species => ==("Adelie"), _)
-		scatter!(ax1, 
-			_[!,:flipper_length_mm], _[!,:bill_length_mm];
-			adelie_style...)
-	end
-	
-	@chain penguins begin
-		filter(:species => ==("Chinstrap"), _)
-		scatter!(ax1, _[!,:flipper_length_mm], _[!,:bill_length_mm];
-			chinstrap_style...)
-	end
-	
-	@chain penguins begin
-		filter(:species => ==("Gentoo"), _)
-		scatter!(ax1, 
-			_[!,:flipper_length_mm], _[!,:bill_length_mm];
-			gentoo_style...)
+	for (i, gdf) in enumerate(groupby(penguins, :species, sort = true))
+    	scatter!(
+			gdf.flipper_length_mm, 
+			gdf.bill_length_mm; 
+			style[gdf.species[1]]...
+		)
 	end
 
 	axislegend("Species", position = :rb)
@@ -146,9 +194,17 @@ end
 
 # ╔═╡ Cell order:
 # ╟─5a547c3d-91ae-40d6-bb5b-4063663d934c
+# ╟─5f11a9cb-8e79-4ff5-a556-7e23ffe6a9ac
 # ╠═1e0a6d46-a758-11eb-209f-b17c4349fa4d
+# ╟─ec127dae-d2ac-42a6-9de6-948feee8c338
 # ╠═a46310e3-53d1-4bab-8b7c-b4bef16efcc9
+# ╟─94f1ce39-a43e-4f25-9c4c-683509861d46
 # ╠═836fd8dd-f920-4975-a6a0-47da575b3eb7
-# ╠═bf10c94e-5b9e-463f-9c7d-c65b237e3166
+# ╟─d5463461-e423-42fc-bb79-9ca68ed04715
+# ╠═d650356c-9bb7-41cf-82f4-f62d1c2d0d56
+# ╟─0fbb46dd-b198-489a-a648-5b0468e1e6f8
 # ╠═d6896864-73bd-4021-a166-d88a062a6293
-# ╠═99e9388f-147e-4c26-af83-6ee32e6a546d
+# ╟─9f5d001f-e02c-4157-84b5-98f54528c97c
+# ╠═0e3dd7e4-aa23-4a32-a127-ea22b978f2e0
+# ╟─29535b0e-62da-4235-b50e-61376362c507
+# ╠═77fcdbe4-7058-4fa4-a2b9-b72ea5f540d0
